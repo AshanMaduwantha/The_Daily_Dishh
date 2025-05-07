4c584240d1bf0843d072824ed10e6e09d0dd1da0
KavinduFndo <kavindusf2003@gmail.com>
2025-05-07 23:02:52 +0530

    update

--- frontend/src/Pages/User/index.jsx
+++ frontend/src/Pages/User/index.jsx
@@ -51,6 +51,7 @@ function User() {
                     role="tab"
                     aria-controls="posts-tab-pane"
                     aria-selected="true"
+                    style={{ backgroundColor: "#fd7e14", borderColor: "#fd7e14", color: "#fff" }}
                   >
                     MY POSTS
                   </button>
@@ -65,6 +66,7 @@ function User() {
                     role="tab"
                     aria-controls="shared-tab-pane"
                     aria-selected="false"
+                    style={{ backgroundColor: "#fd7e14", borderColor: "#fd7e14", color: "#fff" }}
                   >
                     SHARED POSTS
                   </button>
