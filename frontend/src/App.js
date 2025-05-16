a3e4f31b92fd7b58aa28ee8fe474305d7c969cbf
Oshadhie Indudunu <153111583+Oshadhie@users.noreply.github.com>
2025-05-16 21:33:39 +0530

    update of app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./Components/Navbar";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Home from "./Pages/Home";
@@ -10,24 +9,35 @@ import User from "./Pages/User";
 import SharedPosts from "./Pages/SharedPosts";
 import Profile from "./Pages/Profile";
 import UserPosts from "./Pages/UserPosts";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <div className="body">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/user/:userId" element={<UserPosts />} />
-            <Route path="/sharedposts" element={<SharedPosts />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </div>
-      </Router>
+    <Router>
+      <MainApp />
+    </Router>
+  );
+}
+
+// Separate component to access `useLocation` inside Router
+function MainApp() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === "/login" || location.pathname === "/signup";
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+      <div className="body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/user/:userId" element={<UserPosts />} />
+          <Route path="/sharedposts" element={<SharedPosts />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -40,7 +50,7 @@ function App() {
         pauseOnHover
         theme="light"
       />
-    </div>
+    </>
   );
 }
 
